c376c0f565884444259c2472658ccfca62a0e8c8
vusimonama <vusi.muzi6@gmail.com>
2020-10-19 12:16:17 +0200

    Add display numbers

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,8 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Generating report");
-        System.out.println("...........");
+        System.out.println("..........");
+
+        System.out.println("Display numbers");
     }
 }
